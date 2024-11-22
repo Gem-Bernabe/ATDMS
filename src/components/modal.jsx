5a0a7c39effc6d960013c2e445e281e66a2a59cf
akemi0153 <christianjosephmarigmen39@gmail.com>
2024-11-22 14:28:34 +0800

    initialize commit
    New Modal added

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
+  const [isMounted, setIsMounted] = useState(false);
 
-  return ReactDOM.createPortal(
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center p-4 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-        {children}
-        <button
-          onClick={onClose}
-          className="mt-4 w-full text-center py-2 bg-gray-200 text-gray-700 rounded hover:bg-gray-300 focus:outline-none"
+  useEffect(() => {
+    setIsMounted(true);
+    return () => setIsMounted(false);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return typeof document !== "undefined"
+    ? ReactDOM.createPortal(
+        <div
+          className={`fixed inset-0 z-50 flex items-center justify-center ${
+            isOpen ? "block" : "hidden"
+          }`}
         >
-          Close
-        </button>
-      </div>
-    </div>,
-    document.body
-  );
+          <div className="bg-white p-6 rounded shadow-lg">
+            {children}
+            <button
+              onClick={onClose}
+              className="mt-4 w-full text-center py-2 bg-gray-200 text-gray-700 rounded hover:bg-gray-300 focus:outline-none"
+            >
+              Close
+            </button>
+          </div>
+        </div>,
+        document.body
+      )
+    : null;
 };
 
 export default Modal;
